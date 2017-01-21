b55b32d52853c6af5ad0534a19c1f2fafd946129
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-21 15:03:31 +0000

    Add code for special 3*5 multiples
    Added code to deal with special cases where value is a multiple of 3 AND 5

--- JavaScript/lib/fizzbuzz.js
+++ JavaScript/lib/fizzbuzz.js
@@ -4,7 +4,10 @@ class FizzBuzz {
     play(input) {
         var result = input;
 
-        if( input % 3 == 0 ){
+        if( input % (3*5) == 0 ) {
+            result = 'Fizzbuzz';
+        }
+        else if( input % 3 == 0 ){
             result = 'Fizz';
         }
         else if( input % 5 == 0 ){
