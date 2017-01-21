bdf37d41c21b6fecaecf6eb6af3e102c4b01e458
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-21 14:51:21 +0000

    Added test for multples of 3
    Added initial test for multiples of 3

--- JavaScript/test/index.spec.js
+++ JavaScript/test/index.spec.js
@@ -7,5 +7,16 @@ describe('FizzBuzz', function() {
   it('handles simple numbers', function() {
     const fizzbuzz = new FizzBuzz();
     expect(fizzbuzz.play(1)).to.equal(1);
+    expect(fizzbuzz.play(2)).to.equal(2);
+    expect(fizzbuzz.play(101)).to.equal(101);
   });
+
+  it('handles multiples of 3', function() {
+      const fizzbuzz = new FizzBuzz();
+      var maxLoopCount = 1000,
+          loopCount;
+      for( loopCount = 1; loopCount < maxLoopCount; loopCount++ ) {
+          expect(fizzbuzz.play(3*loopCount)).to.equal('Fizz');
+      }
+  })
 });
